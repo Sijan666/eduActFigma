e160ce37e8e66bff988a26370dd27cbdbebb1492
Sijan666 <666majharulislam@gmail.com>
2026-02-11 04:37:16 +0600

    add : common h3 and h4 for reuse

---
+++ src/components/Commonh3.jsx
@@ -0,0 +1,10 @@
+
+const Commonh3 = ({commonh3 , className}) => {
+    return (
+        <>
+            <h4 className={`${className} lg:text-[40px] text-[30px] font-bold`}>{commonh3}</h4>
+        </>
+    )
+}
+
+export default Commonh3
---
+++ src/components/Commonh4.jsx
@@ -0,0 +1,10 @@
+
+const Commonh4 = ({commonh4 , className}) => {
+    return (
+        <>
+            <h4 className={`${className} text-[24px] font-bold`}>{commonh4}</h4>
+        </>
+    )
+}
+
+export default Commonh4
