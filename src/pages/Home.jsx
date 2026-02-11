9422d8376423ca7312785b8c8b8e8dcb99b41eb1
Sijan666 <666majharulislam@gmail.com>
2026-02-12 02:39:37 +0600

    add : about

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@
 import Banner from "../components/sections/Banner";
 import OurCategories from "../components/sections/OurCategories";
 import Discover from "../components/sections/Discover";
+import Aboutus from "../components/sections/Aboutus";
 
 const Home = () => {
     return (
         <>
         <Banner />
+        <Aboutus/>
         <OurCategories />
         <Discover />
         </>
